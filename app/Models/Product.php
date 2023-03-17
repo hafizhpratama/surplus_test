1af0f106eb36d72d675bd3765b013e076d0961d1
Hafizh Pratama <hafizh@alegrium.com>
2023-03-17 14:53:26 +0800

    add model.

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = ['name', 'description', 'enable'];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_product');
+    }
+
+    public function images()
+    {
+        return $this->belongsToMany(Image::class, 'product_image');
+    }
+}
