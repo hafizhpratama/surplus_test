28d5cd7a3c06ecae2f560e3997ddad869c527496
Hafizh Pratama <hafizh@alegrium.com>
2023-03-17 15:21:21 +0800

    add route and index for category.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $table = 'category';
+
     protected $fillable = ['name', 'enable'];
 
     public function products()
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('products', 'ProductController');
+    Route::apiResource('images', 'ImageController');
 });
